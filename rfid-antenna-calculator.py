5a089bf7917f884c04b019f7cdd9b57578b949f1
Stefan Wyss <wyss@superspider.net>
2015-04-19 21:59:23 +0200

    Rlp

--- rfid-antenna-calculator.py
+++ rfid-antenna-calculator.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import math
 import cmath
 
-w = 2.0*math.pi*13.56e6
+w=2.0*math.pi*13.56e6
 
 # antenna topology is
 # Zs(=Rs AND Cs) AND ( Zp(=Cp) OR Zl(=Rl AND L) ) 
@@ -29,32 +29,35 @@ print(" |         |   L")
 print(" |         |   |")
 print(" ---------------")
 
-Rs = 50.0   # Generator U0 output impedance
-Rin = 50.0  # This is the input inpedance of Cs/Cp/Rl/L circuit
-Rl = 2    
-L = 1.5e-6
+U0=1
+Rs=50.0   # Generator U0 output impedance
+Rin=50.0  # This is the input inpedance of Cs/Cp/Rl/L circuit
+Rl=1.5
+L=1.88e-6
+
+Rlp=pow(w*L,2)/Rl
 
 #!!! do not change anython below here !!!#
 print("")
-print("using Rs=%.02f [Ohm], Rl=%.02f [Ohm], L=%.02e [Henry]" % (Rs,Rl,L))
+print("using Rs=%.02f [Ohm], Rl=%.02f (Rlp=%.02e) [Ohm], L=%.02e [Henry]" % (Rs,Rl,Rlp,L))
 print("")
 
 # calculation for Cp (solve quadratic equation)
-a = pow(w,2)*(pow(w,2)*pow(L,2)+pow(Rl,2))
-b = -2*pow(w,2)*L
-c = 1-Rl/Rin
+a=pow(w,2)*(pow(w,2)*pow(L,2)+pow(Rl,2))
+b=-2*pow(w,2)*L
+c=1-Rl/Rin
 
-Cp1 = (- b - math.sqrt(pow(b,2)-4*a*c))/(2*a)
-Cp2 = (- b + math.sqrt(pow(b,2)-4*a*c))/(2*a)
+Cp1=(- b - math.sqrt(pow(b,2)-4*a*c))/(2*a)
+Cp2=(- b + math.sqrt(pow(b,2)-4*a*c))/(2*a)
 
 # calculation for Cs
-Cp = Cp1
-Cs1 = 1-2*pow(w,2)*L*Cp+pow(w,2)*pow(Cp,2)*(pow(w,2)*pow(L,2)+Rl)
-Cs1 = Cs1/(pow(w,2)*(L-Cp*(pow(w,2)*pow(L,2)+pow(Rl,2))))
+Cp=Cp1
+Cs1=1-2*pow(w,2)*L*Cp+pow(w,2)*pow(Cp,2)*(pow(w,2)*pow(L,2)+Rl)
+Cs1=Cs1/(pow(w,2)*(L-Cp*(pow(w,2)*pow(L,2)+pow(Rl,2))))
 
 Cp=Cp2
-Cs2 = 1-2*pow(w,2)*L*Cp+pow(w,2)*pow(Cp,2)*(pow(w,2)*pow(L,2)+Rl)
-Cs2 = Cs2/(pow(w,2)*(L-Cp*(pow(w,2)*pow(L,2)+pow(Rl,2))))
+Cs2=1-2*pow(w,2)*L*Cp+pow(w,2)*pow(Cp,2)*(pow(w,2)*pow(L,2)+Rl)
+Cs2=Cs2/(pow(w,2)*(L-Cp*(pow(w,2)*pow(L,2)+pow(Rl,2))))
 
 # print results
 print("Solution 1: Cp=%.2e Cs=%.2e [F]" % (Cp1,Cs1))
@@ -62,23 +65,25 @@ print("Solution 2: Cp=%.2e Cs=%.2e [F]" % (Cp2,Cs2))
 
 if Cp1<0 or Cs1<0:
     print("-> using solution 2")
-    Cs = Cs2
-    Cp = Cp2
+    Cs=Cs2
+    Cp=Cp2
 else:
     print("-> using solution 1")
-    Cs = Cs1
-    Cp = Cp1
+    Cs=Cs1
+    Cp=Cp1
 
-Zin= Rl/(1-2*pow(w,2)*L*Cp+pow(w,2)*pow(Cp,2)*(pow(w,2)*pow(L,2)+pow(Rl,2)))
-print("Verification: Zin=%.3f [Ohm]"% Zin)
+# define the antenna impedances
+s=complex(0,w)
+Zp=1/(s*Cp)
+Zs=Rs+1/(s*Cs)
+Zl=Rl+s*L
+
+# calculate antenna input impedance
+Zin=Rl/(1-2*pow(w,2)*L*Cp+pow(w,2)*pow(Cp,2)*(pow(w,2)*pow(L,2)+pow(Rl,2)))
+print("Input impedance (without Rs): Zin=%.2f [Ohm]"% Zin)
 
 # calculate antenna current
-s = complex(0,w)
-Zp = 1/(s*Cp)
-Zs = Rs+1/(s*Cs)
-Zl = Rl+s*L
-U0=1
-Zabs = abs(Zs+Zl*Zs/Zp+Zl)
-Il = U0/Zabs
+Zabs=abs(Zs+Zl*Zs/Zp+Zl)
+Il=U0/Zabs
 print("Max. RFID coil (L) current: I=%.3f [A]" % Il)
 
